5bc714a30b847df9ba0a670617a8f49a182df3be
Idaho Edokpayi <idaho@tridentprofessionalservices.com>
2020-02-25 17:36:00 -0500

    added blob storage

--- src/components/storeBlob.ts
+++ src/components/storeBlob.ts
@@ -1,6 +1,36 @@
 import IStoreBlob from "./IStoreBlob";
+import { BlobServiceClient, ContainerItem } from "@azure/storage-blob";
 
-export default async function StoreBlob(props:IStoreBlob): Promise<void>{
-    const { data } = props;
-    console.log(data)
+export default async function StoreBlob(props: IStoreBlob): Promise<void> {
+  const { data } = props;
+  const account = "fileencryptiondemo";
+  const blobServiceClient = new BlobServiceClient(
+    `https://${account}.blob.core.windows.net`
+  );
+
+  
+  const containerName = `demo`;
+  let demoContainer: ContainerItem | null = null;
+  let iter = await blobServiceClient.listContainers();
+  for await (const container of iter) {
+    if (container.name === containerName) {
+      demoContainer = container;
+    }
+  }  
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  if (null === demoContainer) {
+    // create container
+    // Create a container
+
+    const createContainerResponse = await containerClient.create();
+    console.log(
+      `Create container ${containerName} successfully`,
+      createContainerResponse.requestId
+    );
+  }
+
+  const blobName = "newblob" + new Date().getTime();
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+  const uploadBlobResponse = await blockBlobClient.upload(data, Buffer.byteLength(data));
+  console.log("Uploaded data" +JSON.stringify(uploadBlobResponse));
 }
