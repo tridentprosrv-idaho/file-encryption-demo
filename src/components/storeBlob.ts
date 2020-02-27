365d9c9ab316ea9898b1729603da3987ad6c7a31
Idaho Edokpayi <idaho@tridentprofessionalservices.com>
2020-02-27 12:53:02 -0500

    temporary shared access key

--- src/components/storeBlob.ts
+++ src/components/storeBlob.ts
@@ -1,14 +1,24 @@
+import { AnonymousCredential, BlobServiceClient, ContainerItem, newPipeline } from "@azure/storage-blob";
 import IStoreBlob from "./IStoreBlob";
-import { BlobServiceClient, ContainerItem } from "@azure/storage-blob";
 
 export default async function StoreBlob(props: IStoreBlob): Promise<void> {
   const { data } = props;
   const account = "fileencryptiondemo";
+  const accountSas = "?sv=2019-02-02&ss=b&srt=sco&sp=rwdlac&se=2020-02-28T01:37:10Z&st=2020-02-27T17:37:10Z&sip=172.18.108.209&spr=https,http&sig=ibJpF2yO9UYo4YjKLW6TX7lm500F6VWVILKiUNKCmrk%3D";
+  const pipeline = newPipeline(new AnonymousCredential(), {
+    // httpClient: MyHTTPClient, // A customized HTTP client implementing IHttpClient interface
+    retryOptions: { maxTries: 4 }, // Retry options
+    userAgentOptions: { userAgentPrefix: "AdvancedSample V1.0.0" }, // Customized telemetry string
+    keepAliveOptions: {
+      // Keep alive is enabled by default, disable keep alive by setting false
+      enable: false,
+    },
+  });
   const blobServiceClient = new BlobServiceClient(
-    `https://${account}.blob.core.windows.net`
+    `https://${account}.blob.core.windows.net${accountSas}`,
+    pipeline
   );
 
-  
   const containerName = `demo`;
   let demoContainer: ContainerItem | null = null;
   let iter = await blobServiceClient.listContainers();
@@ -16,7 +26,7 @@ export default async function StoreBlob(props: IStoreBlob): Promise<void> {
     if (container.name === containerName) {
       demoContainer = container;
     }
-  }  
+  }
   const containerClient = blobServiceClient.getContainerClient(containerName);
   if (null === demoContainer) {
     // create container
@@ -31,6 +41,9 @@ export default async function StoreBlob(props: IStoreBlob): Promise<void> {
 
   const blobName = "newblob" + new Date().getTime();
   const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-  const uploadBlobResponse = await blockBlobClient.upload(data, Buffer.byteLength(data));
-  console.log("Uploaded data" +JSON.stringify(uploadBlobResponse));
+  const uploadBlobResponse = await blockBlobClient.upload(
+    data,
+    Buffer.byteLength(data)
+  );
+  console.log("Uploaded data" + JSON.stringify(uploadBlobResponse));
 }
